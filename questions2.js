07d13d047f57c76c5390cb54915c1ff3ccafe12a
Bonte Emma BANA <bbonteemma@gmail.com>
2021-08-10 19:42:32 +0200

    adding all

---
+++ questions2.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers for the css quiz
+let questions = [
+    {
+    numb: 1,
+    question: "What does CSS stand for?",
+    answer: "Cascading style sheets",
+    options: [
+      "Hyper Text Preprocessor",
+      "Color and style sheets",
+      "Cascading style sheets",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The property in CSS used to change the background color of an element is:",
+    answer: "background-color",
+    options: [
+      "bgcolor",
+      "color",
+      "background-color",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The CSS property used to control the element's font-size is:",
+    answer: "font-size",
+    options: [
+      "text-style",
+      "text-size",
+      "font-size",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following CSS property is used to set the background image of an element?",
+    answer: "background-image",
+    options: [
+      "background-attachment",
+      "background-image",
+      "background-color",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following property is used as the shorthand property for the padding properties?",
+    answer: "padding",
+    options: [
+      "padding-left",
+      "padding-right",
+      "padding",
+      "All of the above"
+    ]
+  },
+ 
+];
---
+++ questions4.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers for the bootstrap quiz
+let questions = [
+    {
+    numb: 1,
+    question: " Is Bootstrap3 mobile-first?",
+    answer: "True",
+    options: [
+      "True",
+      "False",
+      "Can't say",
+      "May be"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following class in Bootstrap is used to provide a responsive fixed width container?",
+    answer: ".container",
+    options: [
+      ".container-fixed",
+      ".container-fluid",
+      ".container",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following class in bootstrap is used to create a big box for calling extra attention?",
+    answer: " .jumbotron",
+    options: [
+      ".box",
+      ".container",
+      ".container-fluid",
+      ".jumbotron"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following class is used to create a black navigation bar?",
+    answer: ".navbar-inverse",
+    options: [
+      ".navbar-default",
+      ".navbar-inverse",
+      ".navbar-black",
+      ".navbar-dark"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The plugin used to create a cycle through elements as a slideshow is",
+    answer: "carousel",
+    options: [
+      "slideshow",
+      "scrollspy",
+      "carousel",
+      "None of the above"
+    ]
+  },
+ 
+];
